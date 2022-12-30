246add230f638abc881380ede04dc4c73a48a508
vijayaprm <vijayapratapreddy.m@gmail.com>
2022-12-30 10:47:40 +0530

    hosting

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php      
     $host = "localhost";  
-    $user = "root";  
-    $password = "";  
-    $db_name = "login";  
+    $user = "id19933818_root";  
+    $password = "HNj2d758ZD6gA@";  
+    $db_name = "id19933818_login";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
